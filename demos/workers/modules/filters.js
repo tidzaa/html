879f5280a4cea46c632193626a0073fa08de6ba9
H <103602300+tidzaa@users.noreply.github.com>
2023-02-08 20:47:32 -0500

    Update filters.js

--- demos/workers/modules/filters.js
+++ demos/workers/modules/filters.js
@@ -4,8 +4,8 @@ export function grayscale({ data: d }) {
   for (let i = 0; i < d.length; i += 4) {
     const [r, g, b] = [d[i], d[i + 1], d[i + 2]];
 
-    // CIE luminance for the RGB
-    // The human eye is bad at seeing red and blue, so we de-emphasize them.
+    // CIE luminance for RGB
+    // The human eye is bad at seeing red and blue, so we unemphasize them.
     d[i] = d[i + 1] = d[i + 2] = 0.2126 * r + 0.7152 * g + 0.0722 * b;
   }
 };
